5f457908d0eb3f857d62ce697a32c014ce846910
Somanath Chowdary <somanath.chowdary@ymail.com>
2020-05-21 08:00:54 +0530

    Add missing fields to Artist model as db migration.

--- app.py
+++ app.py
@@ -65,11 +65,18 @@ class Artist(db.Model):
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
+    # Using array to store multiple values of genres
+    genres = db.Column(db.ARRAY(db.String()))
+    # Max url length may range upto 2048 characters
+    image_link = db.Column(db.String(2048),
+                           default="https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80"
+                          )
+    facebook_link = db.Column(db.String(2048))
+    # TODO:[COMPLETED] implement any missing fields, as a database migration using Flask-Migrate
+    website = db.Column(db.String(2048))
+    seeking_venue = db.Column(db.Boolean, nullable=False)
+    seeking_description = db.Column(db.String(256), nullable=True)
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
